b24ad836a2f2f6fa4a9917659bf95c390841cfb8
Ace <121753552+acejobbie@users.noreply.github.com>
2024-11-11 16:33:30 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,24 +5,24 @@ You don't have to set this if you deploy using heroku because you can simply set
 
 const session = process.env.SESSION || '';
 
-const prefix = process.env.PREFIX || '.';
+const prefix = process.env.PREFIX || '#';
 const mycode = process.env.CODE || "254";
-const author = process.env.STICKER_AUTHOR || 'fortunatus';
-const packname = process.env.PACKNAME || 'dreaded md2 🤖';
-const dev = process.env.DEV || '254114018035';
+const author = process.env.STICKER_AUTHOR || 'ན🏴‍☠️⃝🅐ϲԑ 𝚔𝖎𝐫қ ❬𝕯❭';
+const packname = process.env.PACKNAME || '𝙸 αɱ ค̡ƈє™⟬𝟭⟭';
+const dev = process.env.DEV || '254726856795';
 const DevDreaded = dev.split(",");
-const botname = process.env.BOTNAME || 'DREADED';
-const mode = process.env.MODE || 'public';
+const botname = process.env.BOTNAME || '𒋨🏴‍☠️⃝𝘼𝘾𝙀☠️𝘽𝙊𝙏⃝𒋨🉑';
+const mode = process.env.MODE || 'private';
 const gcpresence = process.env.GC_PRESENCE || 'false';
-const antionce = process.env.ANTIVIEWONCE || 'true';
+const antionce = process.env.ANTIVIEWONCE || 'false';
 const sessionName = "session";
-const presence = process.env.WA_PRESENCE || 'online';
+const presence = process.env.WA_PRESENCE || 'unavailable';
 
-const antitag = process.env.ANTITAG || 'true';
-const antidelete = process.env.ANTIDELETE || 'true';
+const antitag = process.env.ANTITAG || 'false';
+const antidelete = process.env.ANTIDELETE || 'false';
 const autoview = process.env.AUTOVIEW_STATUS || 'true';
-const autolike = process.env.AUTOLIKE_STATUS || 'true';
-const autoread = process.env.AUTOREAD || 'true';
+const autolike = process.env.AUTOLIKE_STATUS || 'false';
+const autoread = process.env.AUTOREAD || 'false';
 const autobio = process.env.AUTOBIO || 'false';
 
 module.exports = {
